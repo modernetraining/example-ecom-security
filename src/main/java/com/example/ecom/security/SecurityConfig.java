d96737e6243f0712852b29ba477313adae95daae
Matthew Campbell <matt@moderne.io>
2026-01-14 21:21:25 -0500

    Revert to starting point for migration practice

--- src/main/java/com/example/ecom/security/SecurityConfig.java
+++ src/main/java/com/example/ecom/security/SecurityConfig.java
@@ -1,17 +1,16 @@
 package com.example.ecom.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
@@ -20,6 +19,5 @@ public class SecurityConfig {
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
-        return http.build();
     }
 }
